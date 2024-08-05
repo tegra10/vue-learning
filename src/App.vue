016fe67659826a6f90a66d3c4dee8946151f61f8
tegra10 <tegkit30@gmail.com>
2024-08-05 16:28:34 +0100

    2eme commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,21 @@
-<script setup></script>
 <template>
-  <p>Hello world</p>
+    <p>Hello world</p>
+    <p>{{ nom }}</p>
+    <button @click="inc">{{ count }}</button>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            nom: "tegra",
+            count: 0
+        };
+    },
+    methods: {
+        inc() {
+            this.count++;
+            console.log(this.count)
+        }
+    }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import eruda from 'eruda'
 
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+eruda.init()
